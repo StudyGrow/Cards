8951129a58adac6efb449ee394d3654997d886e9
jungerjunge2 <rastoder.erdzan@rwth-aachen.de>
2020-02-08 00:33:15 +0100

    work

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
 
 
 const app = express();
+// const router = express.Router();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// router.get('/',function(req,res){
+//   res.sendFile('Karteikarten.html');
+//   //__dirname : It will resolve to your project folder.
+// });
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
-//app.use('/', routes);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const path = require('path');
+const { body, validationResult } = require('express-validator/check');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('form', { title: 'Registration form' });
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/../Karteikarten.html'));
+  //__dirname : It will resolve to your project folder.
 });
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.render('form', { title: 'Registration form' });
-});
+
+
+// router.get('/', (req, res) => {
+//   // res.render('form', { title: 'Registration form' });
+//   res.sendFile('Karteikarten.html')
+// });
+
+router.post('/',
+  [
+    body('exampleFormControlTextarea1')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a name'),
+    body('exampleFormControlTextarea1')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      res.send('Thank you for your registration!');
+    } else {
+      res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 module.exports = router;
