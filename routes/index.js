9a2c8cdd5591a50350b7a31a59dcbff65203e90e
jungerjunge2 <rastoder.erdzan@rwth-aachen.de>
2020-02-07 23:47:31 +0100

    web

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('form', { title: 'Registration form' });
+});
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.render('form', { title: 'Registration form' });
+});
+module.exports = router;
