ec911d181feec62ff5c6cc5b95aaef4a974eca57
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-02-08 01:45:28 +0100

    frontend karten laden und hinzufügen mit Ajax

--- app.js
+++ app.js
@@ -9,6 +9,33 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+const Card = require('./models/card');
+
+app.get('/karten/:vl',(req,res)=>{
+    Card.find({vorlesung:req.params.vl},(err,cards)=>{//suche in der Datenbank nach allen Karten die der Vorlesung vl zugeordnet sind
+      if(err){
+        console.log(err);
+      }else{
+        res.send(cards);//sende diese an den client
+      }
+    });
+});
+
+app.post('addCard',(req,res)=>{
+    var card = new Card(); //erstelle neue Karte
+    card.vorlesung = req.body.vorlesung;
+    card.thema = req.body.thema;
+    card.content = req.body.content;
+
+    card.save((err,c)=>{ //speichere sie in der Datenbank
+        if(err){
+            console.log(err);
+        }else{
+            res.send(c);
+        }
+    });
+});
 //app.use('/', routes);
 
 app.use(bodyParser.urlencoded({ extended: true }));
