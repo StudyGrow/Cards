4a4f656116f45023067ed564bc919d8d07ec4b28
jungerjunge2 <rastoder.erdzan@rwth-aachen.de>
2020-02-09 01:25:15 +0100

    change port

--- start.js
+++ start.js
@@ -15,7 +15,7 @@ mongoose.connection
 
   require('./models/Registration');
   const app = require('./app');
-const server = app.listen(3000, () => {
+const server = app.listen(80, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
 
