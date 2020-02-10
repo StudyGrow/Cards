01041b846c6be33263a7b494a953fb2a6c024e9a
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-02-11 00:34:29 +0100

    NOHTTPS

--- start.js
+++ start.js
@@ -8,15 +8,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require("helmet");
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/chain.pem', 'utf8');
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/chain.pem', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
 
 mongoose.connect(process.env.DATABASE, { useMongoClient: true });
 mongoose.Promise = global.Promise;
@@ -31,18 +31,18 @@ mongoose.connection
   require('./models/Registration');
   require('./models/Vorlesung');
   const app = require('./app');
-  app.use(helmet()); 
+  //app.use(helmet()); 
   const httpServer = http.createServer(app);
-  const httpsServer = https.createServer(credentials, app);
+  //const httpsServer = https.createServer(credentials, app);
 
   // httpServer.listen(80, () => {
   //   console.log('HTTP Server running on port 80');
   // });
-  app.use(helmet()); 
+  //app.use(helmet()); 
 
-  httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
-  });
+  //httpsServer.listen(443, () => {
+  //  console.log('HTTPS Server running on port 443');
+  //});
 const server = app.listen(80, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
