54faa32b4205b1ed8c5a531a9ace9bb006e142a3
jungerjunge2 <rastoder.erdzan@rwth-aachen.de>
2020-02-10 20:51:19 +0100

    https

--- start.js
+++ start.js
@@ -1,7 +1,21 @@
 
 require('dotenv').config();
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 const mongoose = require('mongoose');
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 mongoose.connect(process.env.DATABASE, { useMongoClient: true });
 mongoose.Promise = global.Promise;
@@ -16,6 +30,17 @@ mongoose.connection
   require('./models/Registration');
   require('./models/Vorlesung');
   const app = require('./app');
-const server = app.listen(80, () => {
-  console.log(`Express is running on port ${server.address().port}`);
-});
+
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
+
+  httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+  });
+  
+  httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+  });
+// const server = app.listen(80, () => {
+//   console.log(`Express is running on port ${server.address().port}`);
+// });
