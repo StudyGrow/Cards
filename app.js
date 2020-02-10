88a980fc6955a368c8e57a4d46352cfbfc611d41
jungerjunge2 <rastoder.erdzan@rwth-aachen.de>
2020-02-10 21:47:21 +0100

    htpps

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
+const helmet = require("helmet");
 
 
 const app = express();
