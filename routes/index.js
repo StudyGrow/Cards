78327aa7958549193c0fd8f7adb68f5d4227c4c8
jungerjunge2 <rastoder.erdzan@rwth-aachen.de>
2020-02-10 22:04:41 +0100

    htpps

--- routes/index.js
+++ routes/index.js
@@ -20,12 +20,7 @@ router.get('/',function(req,res){
     }
     }); 
 });
-router.get('*', function(req, res) {  
-  res.redirect('https://' + req.headers.host + req.url);
 
-  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
-  // res.redirect('https://example.com' + req.url);
-})
 router.get('/kategorien',function(req,res){
   // console.log(typeof(req.params.vl))
   Vorlesung.find((err,vls)=>{
@@ -143,5 +138,10 @@ router.post('/updateCard',function(req,res){
 });
 
 });
+router.get('*', function(req, res) {  
+  res.redirect('https://' + req.headers.host + req.url);
 
+  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+  // res.redirect('https://example.com' + req.url);
+})
 module.exports = router;
