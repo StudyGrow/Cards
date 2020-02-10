2613352c4c431ee0385f75593ba7e57f948345aa
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-02-10 21:28:25 +0100

    hotfix

--- start.js
+++ start.js
@@ -7,15 +7,15 @@ const https = require('https');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/chain.pem', 'utf8');
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/chain.pem', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
 
 mongoose.connect(process.env.DATABASE, { useMongoClient: true });
 mongoose.Promise = global.Promise;
@@ -32,15 +32,15 @@ mongoose.connection
   const app = require('./app');
 
   const httpServer = http.createServer(app);
-  const httpsServer = https.createServer(credentials, app);
+  //const httpsServer = https.createServer(credentials, app);
 
   httpServer.listen(80, () => {
     console.log('HTTP Server running on port 80');
   });
   
-  httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
-  });
+  // httpsServer.listen(443, () => {
+  //   console.log('HTTPS Server running on port 443');
+  // });
 // const server = app.listen(80, () => {
 //   console.log(`Express is running on port ${server.address().port}`);
 // });
