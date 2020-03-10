7b86cc6c41ae97187f139e636495037e30c8e4e9
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-03-10 15:51:29 +0100

    added new homeroute with Vorlesungen

--- angular-cards/src/app/components/add-lecture-form/add-lecture-form.component.ts
+++ angular-cards/src/app/components/add-lecture-form/add-lecture-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 @Component({
-  selector: 'app-add-lecture-form',
-  templateUrl: './add-lecture-form.component.html',
-  styleUrls: ['./add-lecture-form.component.css']
+  selector: "app-add-lecture-form",
+  templateUrl: "./add-lecture-form.component.html",
+  styleUrls: ["./add-lecture-form.component.css"]
 })
 export class AddLectureFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit(f: NgForm) {
+    console.log(f.value.name);
+    console.log(f.value.abrv);
   }
-
 }
--- angular-cards/src/app/components/lectures/lectures.component.ts
+++ angular-cards/src/app/components/lectures/lectures.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Vorlesung } from "../../models/Vorlesung";
 @Component({
-  selector: 'app-lectures',
-  templateUrl: './lectures.component.html',
-  styleUrls: ['./lectures.component.css']
+  selector: "app-lectures",
+  templateUrl: "./lectures.component.html",
+  styleUrls: ["./lectures.component.css"]
 })
 export class LecturesComponent implements OnInit {
+  lectures: Vorlesung[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  setLink(lecture: Vorlesung) {
+    return "/vorlesung/" + lecture.abrv;
   }
-
 }
