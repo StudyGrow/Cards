0797d0ed914b6c37d75bed7a76223c92f82d26d2
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-04-02 16:15:48 +0200

    i

--- angular-cards/src/app/app.component.ts
+++ angular-cards/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
-
+declare var $: any;
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  title = "Cards";
+}
