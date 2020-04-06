e8f3aa3ea79d0ab5a2c2c7e542722145d6660e31
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-04-06 21:09:28 +0200

    fixes activeCardIndex updates

--- angular-cards/src/app/components/carousel/carousel.component.ts
+++ angular-cards/src/app/components/carousel/carousel.component.ts
@@ -32,7 +32,7 @@ export class CarouselComponent implements OnInit {
     this.carousel.nextSlide();
   }
   cards: Card[]; //array of all the cards
-  activeSlide: number;
+  activeSlide: number = 0;
   title: string;
   addComponentHidden: boolean;
   formShow: boolean;
@@ -119,7 +119,7 @@ export class CarouselComponent implements OnInit {
       this.carousel.nextSlide();
     }
   }
-  onSlide(slideEvent: NgbSlideEvent) {
-    this.cardsService.setActiveCardIndex(parseInt(slideEvent.current));
+  onSlide(slideEvent) {
+    this.cardsService.setActiveCardIndex(parseInt(slideEvent.relatedTarget));
   }
 }
