221f491966d2224aea2ccf3dd5569adf2409a66a
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-05-11 12:53:51 +0200

    added comments

--- angular-cards/src/app/services/cards.service.ts
+++ angular-cards/src/app/services/cards.service.ts
@@ -10,27 +10,33 @@ import { Router } from "@angular/router";
   providedIn: "root",
 })
 export class CardsService {
-  private cards$: BehaviorSubject<Card[]>;
+  //contains lecture abreviation of the current lecutre
   private abrv: string;
+  //loads cards once from the server and whenever lecture changes
+  //and provides them as an Observable
+  private cards$: BehaviorSubject<Card[]>;
+  //contains all http errors this Subject should be provided in the http service not here
   private errors$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(
     []
   );
+  //provides a Subject to set a new index on which card the carousel should show
   private newCardIndex$: BehaviorSubject<number> = new BehaviorSubject<number>(
     0
   );
+  //provides a Subject of the index of the card that is currently shown
+  //only the carousel shoul set nex values for this subject
   private activeCardIndex$: BehaviorSubject<number> = new BehaviorSubject<
     number
   >(0);
-  private cards: Card[];
 
   constructor(
-    private httpService: HttpService,
-    private statesService: StatesService,
-    private router: Router
+    private httpService: HttpService, //to make calls to the server
+    private statesService: StatesService, //for setting the loading state
+    private router: Router //used to get the lecture abreviation from the route
   ) {}
 
   getCards(): Observable<Card[]> {
-    let abrv = this.router.url.split(/vorlesung\//)[1];
+    let abrv = this.router.url.split(/vorlesung\//)[1]; //get the lecture abreviation from the route
     if (this.cards$ && this.abrv == abrv) {
       //cards were already loaded for this lecture
       return this.cards$.asObservable();
@@ -50,21 +56,26 @@ export class CardsService {
     }
   }
 
-  getErrors() {
+  getErrors(): Observable<string[]> {
     return this.errors$.asObservable();
   }
+
+  //removes a specific error from the error array
   removeError(index: number) {
     let errors = this.errors$.getValue();
-    errors.splice(index, 1);
+    errors.splice(index, 1); //remove error at position index
     this.errors$.next(errors);
   }
+
   updateCard(card: Card, index: number): Observable<any> {
     this.statesService.setLoadingState(true);
+    //send update to server using http service
     return this.httpService.updateCard(card).pipe(
       tap(
         (resp) => {
           this.statesService.setLoadingState(false);
-          this.statesService.setFormMode("reset");
+          this.statesService.setFormMode("reset"); //reset form to its previous state
+          //update subject
           let cards = this.cards$.getValue();
           cards[index] = card;
           this.cards$.next(cards);
@@ -78,24 +89,37 @@ export class CardsService {
       )
     );
   }
+
   addCard(card: Card): Observable<any> {
     this.statesService.setLoadingState(true);
+    //send new card to server using http service
     return this.httpService.addCard(card).pipe(
-      tap((response) => {
-        this.statesService.setLoadingState(false);
-        card._id = response.body;
-        let cards = this.cards$.getValue();
-        cards.push(card);
-        this.cards$.next(cards);
-        setTimeout(() => {
-          //show new card timeout needed because the carousel needs time to refresh
-          //its view
-          this.setNewCardIndex(cards.length - 1);
-        }, 100);
-      })
+      tap(
+        (response) => {
+          this.statesService.setLoadingState(false);
+          card._id = response.body; //set id received from server response
+          //upate subject
+          let cards = this.cards$.getValue();
+          cards.push(card);
+          this.cards$.next(cards);
+
+          setTimeout(() => {
+            //show new card timeout needed because the carousel needs time to refresh
+            //its view
+            this.setNewCardIndex(cards.length - 1);
+          }, 100);
+        },
+        (error) => {
+          let errors = this.errors$.getValue();
+          errors.push(error.error);
+          this.errors$.next(errors);
+          this.statesService.setLoadingState(false);
+        }
+      )
     );
   }
   goNext() {
+    //show the next slide index
     let index = this.newCardIndex$.getValue();
     index++;
     if (index >= this.cards$.getValue().length) {
@@ -104,6 +128,7 @@ export class CardsService {
     this.newCardIndex$.next(index);
   }
   goPrev() {
+    //show the next slide index
     let index = this.newCardIndex$.getValue();
     index--;
     if (index < 0) {
@@ -111,15 +136,19 @@ export class CardsService {
     }
     this.newCardIndex$.next(index);
   }
+  //only the carousel should be subscribed to this
   getNewCardIndex(): Observable<number> {
     return this.newCardIndex$.asObservable();
   }
+  //use this function to tell the carousel to go to a specific slide i
   setNewCardIndex(i: number) {
     this.newCardIndex$.next(i);
   }
+  //only the carousel should call this method (on the sliding event)
   setActiveCardIndex(i: number) {
     this.activeCardIndex$.next(i);
   }
+  //subsribe to this function to always get the index of the card that is currently shown
   getActiveCardIndex(): Observable<number> {
     return this.activeCardIndex$.asObservable();
   }
