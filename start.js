17b4e8fb78266a5b786add6e1c6c52e6f112982e
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-05-20 17:23:55 +0200

    load bootstrap from cdn

--- start.js
+++ start.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
 
-const fs = require("fs");
+// const fs = require("fs");
 const http = require("http");
-const https = require("https");
-const express = require("express");
+// const https = require("https");
+// const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
-const kill = require('./node_modules/kill-port');
-const port = 8080
-const portSSL = 443
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/rwth-aachen.tk/chain.pem', 'utf8');
+// const kill = require("./node_modules/kill-port");
+// const port = 8080;
+// const portSSL = 443;
+// const privateKey = fs.readFileSync("/etc/letsencrypt/live/rwth-aachen.tk/privkey.pem", "utf8");
+// const certificate = fs.readFileSync("/etc/letsencrypt/live/rwth-aachen.tk/cert.pem", "utf8");
+//const ca = fs.readFileSync("/etc/letsencrypt/live/rwth-aachen.tk/chain.pem", "utf8");
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
 
 const databaseConfig = require("./config/database");
 
@@ -43,16 +43,16 @@ require("./models/User");
 const app = require("./app");
 app.use(helmet());
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80, () => {
-   console.log('HTTP Server running on port 80');
+  console.log("HTTP Server running on port 80");
 });
 app.use(helmet());
 
-httpsServer.listen(443, () => {
- console.log('HTTPS Server running on port 443');
-});
+// httpsServer.listen(443, () => {
+//   console.log("HTTPS Server running on port 443");
+// });
 // const server = app.listen(80, () => {
 //   console.log(`Express is running on port ${server.address().port}`);
 // });
