f4eaa5cdc94fcff95b8d43af7a74fd46629b1928
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-05-26 23:19:16 +0200

    send message to client when not authenticated

--- routes/User.js
+++ routes/User.js
@@ -40,7 +40,7 @@ function auth(req, res, next) {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.status(400).send();
+    res.status(400).send("login required"); //send message to client
   }
 }
 router.get("/info", auth, (req, res) => {
