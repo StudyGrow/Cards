82d24b45d4f4eb7e2735c5c1dc7ae132462af51f
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-07-20 10:48:54 +0200

    add env variable to test if dev or prod mode

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     proxy: true,
-    cookie: { secure: false }, //secure needs to be set to true here
+    cookie: { secure: process.env.NODE_ENV.indexOf("development") > -1 ? false : true }, //secure needs to be set to true for production here
 
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
