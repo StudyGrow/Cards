434f6a2598ca05791992941d1436925b58a5909c
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-07-29 14:02:23 +0200

    fix node_env

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     proxy: true,
-    cookie: { secure: process.env.NODE_ENV == "development" ? false : true }, //secure needs to be set to true for production here
+    cookie: {
+      secure:
+        process.env.NODE_ENV && process.env.NODE_ENV.indexOf("development") > -1 ? false : true,
+    }, //secure needs to be set to true for production here
 
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
