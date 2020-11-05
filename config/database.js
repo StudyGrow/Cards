87e3568f891358df9218515d8ed8724360b084be
lakhoune <38857665+lakhoune@users.noreply.github.com>
2020-11-05 12:47:10 +0100

    comments

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+//config of database
+
 module.exports = {
   database: `mongodb+srv://admin:admin@cluster0-eyxul.gcp.mongodb.net/${
     process.env.NODE_ENV && process.env.NODE_ENV.indexOf("development") > -1 ? "test" : "production"
