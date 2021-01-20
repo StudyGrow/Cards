74073f6156b164edc73d4f13161d9ab8e373ba9d
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-01-20 18:54:32 +0100

    backend for multiple choice cards done for adding and updating

--- routes/Cards.js
+++ routes/Cards.js
@@ -1,15 +1,15 @@
 //handles all card specific routes
-const express = require('express');
-const { check, query, validationResult } = require('express-validator'); //uses to validate requests
+const express = require("express");
+const { check, query, validationResult } = require("express-validator"); //uses to validate requests
 const router = express.Router();
 
 //route to get the cards from a specific lecture
 router.get(
-  '/',
+  "/",
   [
-    query('abrv')
+    query("abrv")
       .isLength({ min: 3, max: 7 })
-      .withMessage('Lecture abreviation must be between 3 and 7 characters'),
+      .withMessage("Lecture abreviation must be between 3 and 7 characters"),
   ],
   (req, res) => {
     const errors = validationResult(req);
@@ -35,11 +35,11 @@ router.get(
 
 //returns cards as well as metadata
 router.get(
-  '/data',
+  "/data",
   [
-    query('abrv')
+    query("abrv")
       .isLength({ min: 3, max: 7 })
-      .withMessage('Lecture abreviation must be between 3 and 7 characters'),
+      .withMessage("Lecture abreviation must be between 3 and 7 characters"),
   ],
   (req, res) => {
     const errors = validationResult(req);
@@ -75,23 +75,25 @@ router.get(
 
 //Add card to the database
 router.post(
-  '/new',
+  "/new",
   [
-    check('card.abrv')
+    check("card.abrv")
       .isLength({ min: 3, max: 7 })
-      .withMessage('Vorlesung Abkürzung ugültig (muss zwischen 3 und 7 Zeichen enthalten)'),
-    check('card.thema')
+      .withMessage(
+        "Vorlesung Abkürzung ugültig (muss zwischen 3 und 7 Zeichen enthalten)"
+      ),
+    check("card.thema")
       .isLength({
         min: 3,
         max: 500,
       })
-      .withMessage('Thema muss zwischen 3 und 500 Zeichen enthalten'),
-    check('card.content')
+      .withMessage("Thema muss zwischen 3 und 500 Zeichen enthalten"),
+    check("card.content")
       .isLength({
         min: 1,
         max: 1000,
       })
-      .withMessage('Inhalt darf nicht mehr als 1000 Zeichen enthalten'),
+      .withMessage("Inhalt darf nicht mehr als 1000 Zeichen enthalten"),
   ],
   (req, res) => {
     const errors = validationResult(req);
@@ -112,33 +114,149 @@ router.post(
   }
 );
 
+//Add MultipleChoiceCard to the database, body passed should be of form:
+// {
+//   "multipleChoiceCard": {
+//     MONGODBCARDMODELDATA
+//   }
+// }
+
+router.post(
+  "/newMultipleChoiceCard",
+  [
+    check("multipleChoiceCard.answers")
+      .isArray({ min: 1, max: 4 })
+      .withMessage("At least one answer and maximum 4"),
+    check("multipleChoiceCard.cardDeckID")
+      .exists()
+      .withMessage("Provide cardDeck (lecture) id"),
+    check("multipleChoiceCard.question")
+      .isLength({
+        min: 1,
+        max: 1000,
+      })
+      .exists()
+      .withMessage("Provide a question"),
+    check("multipleChoiceCard.answers.*.option")
+      .isLength({
+        min: 1,
+        max: 1000,
+      })
+      .exists()
+      .withMessage("Provide answer that should be at most 1000 long"),
+    check("multipleChoiceCard.answers.*.correct")
+      .exists()
+      .withMessage("Should specify if option correct or false"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      req.services.cards.addMultipleChoiceCard(
+        req.body.multipleChoiceCard,
+        req.user,
+        (err, card) => {
+          if (err) {
+            res.status(500).send(err.message);
+          } else {
+            console.log(card);
+            res.json(card);
+          }
+        }
+      );
+    }
+  }
+);
+
 // router.get("/renew", (req, res) => {
 //   req.services.cards.renew();
 //   res.send();
 // });
 
-//Update Card in the database
+//Update MultipleChoiceCard to the database, body passed should be of form:
+// {
+//   "multipleChoiceCard": {
+//     MONGODBCARDMODELDATA
+//   }
+// }
 router.put(
-  '/update',
+  "/updateMultipleChoiceCard",
   [
-    check('card._id')
-      .isLength({
-        min: 1,
-        max: 500,
-      })
-      .withMessage('_id der Karte muss angegeben werden'),
-    check('card.thema')
-      .isLength({
-        min: 3,
-        max: 500,
-      })
-      .withMessage('Thema muss zwischen 3 und 500 Zeichen enthalten'),
-    check('card.content')
+    check("multipleChoiceCard.answers")
+      .isArray({ min: 1, max: 4 })
+      .withMessage("At least one answer and maximum 4"),
+    check("multipleChoiceCard.cardDeckID")
+      .exists()
+      .withMessage("Provide cardDeck (lecture) id"),
+    check("multipleChoiceCard._id")
+      .exists()
+      .withMessage("Provide id of ultipleChoiceCard"),
+    check("multipleChoiceCard.question")
       .isLength({
         min: 1,
         max: 1000,
       })
-      .withMessage('Inhalt darf nicht mehr als 1000 Zeichen enthalten'),
+      .exists()
+      .withMessage("Provide a question"),
+    check("multipleChoiceCard.answers.*.option")
+      .isLength({
+        min: 1,
+        max: 1000,
+      })
+      .exists()
+      .withMessage("Provide answer that should be at most 1000 long"),
+    check("multipleChoiceCard.answers.*.correct")
+      .exists()
+      .withMessage("Should specify if option correct or false"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({
+        errors: errors.array(),
+      });
+    } else {
+      req.services.cards.updateMultipleChoiceCard(
+        req.body.multipleChoiceCard,
+        req.user,
+        (err, card) => {
+          if (err) {
+            res.status(422).send(err.message);
+          } else {
+            res.status(200).send(card);
+          }
+        }
+      );
+    }
+  }
+);
+
+//Update Card in the database
+router.put(
+  "/update",
+  [
+    check("card._id")
+      .isLength({
+        min: 1,
+        max: 500,
+      })
+      .withMessage("_id der Karte muss angegeben werden"),
+    check("card.thema")
+      .isLength({
+        min: 3,
+        max: 500,
+      })
+      .withMessage("Thema muss zwischen 3 und 500 Zeichen enthalten"),
+    check("card.content")
+      .isLength({
+        min: 1,
+        max: 1000,
+      })
+      .withMessage("Inhalt darf nicht mehr als 1000 Zeichen enthalten"),
   ],
   (req, res) => {
     const errors = validationResult(req);
@@ -157,30 +275,35 @@ router.put(
     }
   }
 );
-router.put('/vote', check('id').not().isEmpty().withMessage('Karten Id benötigt'), (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(422).json({
-      errors: errors.array(),
+
+router.put(
+  "/vote",
+  check("id").not().isEmpty().withMessage("Karten Id benötigt"),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    let vote = parseInt(req.body.value);
+    req.services.votes.castVote(req, (err, result) => {
+      if (err) {
+        res.status(422).send(err.message);
+      } else {
+        res.status(200).json(result);
+      }
     });
   }
-
-  let vote = parseInt(req.body.value);
-  req.services.votes.castVote(req, (err, result) => {
-    if (err) {
-      res.status(422).send(err.message);
-    } else {
-      res.status(200).json(result);
-    }
-  });
-});
+);
 
 router.get(
-  '/votes',
+  "/votes",
   [
-    query('abrv')
+    query("abrv")
       .isLength({ min: 3, max: 7 })
-      .withMessage('Lecture abreviation must be between 3 and 7 characters'),
+      .withMessage("Lecture abreviation must be between 3 and 7 characters"),
   ],
   (req, res) => {
     const errors = validationResult(req);
@@ -190,13 +313,17 @@ router.get(
       });
     }
     if (req.isAuthenticated()) {
-      req.services.votes.getVotesByLectureAbrv(req.query.abrv, req.user._id, (err, votes) => {
-        if (err) {
-          res.status(422).send(err.message);
-        } else {
-          res.status(200).send(votes);
+      req.services.votes.getVotesByLectureAbrv(
+        req.query.abrv,
+        req.user._id,
+        (err, votes) => {
+          if (err) {
+            res.status(422).send(err.message);
+          } else {
+            res.status(200).send(votes);
+          }
         }
-      });
+      );
     } else {
       res.status(200).send([]);
     }
