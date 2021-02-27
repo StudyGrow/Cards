14081ee2de36a01abfa1f970155f94bfac57d404
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-02-27 13:46:14 +0100

    small fixes

--- angular-cards/src/app/cards/carousel/carousel.component.ts
+++ angular-cards/src/app/cards/carousel/carousel.component.ts
@@ -273,7 +273,7 @@ export class CarouselComponent implements OnInit, OnDestroy {
     if (
       this.carousel &&
       this.cardsToShowInCarousel &&
-      this.cardsToShowInCarousel.length > 1 &&
+      this.cardsToShowInCarousel.length >= 1 &&
       this.formMode != 'edit'
     ) {
       let state = { ...this.carouselInfo$.getValue() };
@@ -318,14 +318,11 @@ export class CarouselComponent implements OnInit, OnDestroy {
         newChunk = this.cardsToShowInCarousel.concat(newChunk);
         newChunk = [...new Set(newChunk)];
         let sortedReference = state.allCardsSorted;
-        newChunk.sort((a: any, b: any) => {
-          sortedReference.indexOf(a) - sortedReference.indexOf(b);
-        });
-        this.cardsToShowInCarousel = await newChunk;
+        newChunk.sort((a: any, b: any) => sortedReference.indexOf(a) - sortedReference.indexOf(b));
 
         setTimeout(() => {
           this.carousel.prev();
-        }, 100);
+        }, 150);
       }
     } else {
       this.showRejection();
