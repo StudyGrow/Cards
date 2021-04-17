774bac35d9f7e7b8cfd17536efabd24f58d069f0
lakhoune <38857665+lakhoune@users.noreply.github.com>
2021-04-17 16:30:58 +0200

    optimize function

--- angular-cards/src/app/cards/carousel/carousel.component.ts
+++ angular-cards/src/app/cards/carousel/carousel.component.ts
@@ -365,25 +365,6 @@ export class CarouselComponent implements OnInit, OnDestroy {
         this.carousel.select(String(this.cardsToShowInCarousel.indexOf(state.currentCard) + 1));
       } else if (currentCardIndexAccordingToAllCard == indexOfLastCardInAllCards) {
         this.showRejection();
-        // var begin = 0;
-        // var end = 0;
-        // if (begin + this.chunkSize > { ...this.carouselInfo$.getValue() }.allCardsSorted.length) {
-        //   end = { ...this.carouselInfo$.getValue() }.allCardsSorted.length;
-        // } else {
-        //   end = begin + this.chunkSize;
-        // }
-        // var newChunk = { ...this.carouselInfo$.getValue() }.allCardsSorted.slice(begin, end);
-        // newChunk = await this.cardsToShowInCarousel.concat(newChunk);
-        // newChunk = [...new Set(newChunk)];
-        // var sortedReference = { ...this.carouselInfo$.getValue() }.allCardsSorted;
-        // newChunk = await newChunk.sort(function (a, b) {
-        //   return sortedReference.indexOf(a) - sortedReference.indexOf(b);
-        // });
-        // this.cardsToShowInCarousel = await newChunk;
-        // setTimeout(() => {
-        //   document.getElementById('slide-' + String(this.cardsToShowInCarousel.length - 1)).classList.remove('active');
-        //   this.carousel.select('0');
-        // }, 100);
       } else {
         let begin = state.allCardsSortedAndFiltered.indexOf(state.currentCard) + 1;
         let end = 0;
@@ -463,7 +444,7 @@ export class CarouselComponent implements OnInit, OnDestroy {
       const card = currCarouselInfo.allCardsSorted?.find((card) => card._id === newCard._id);
       if (!card) return console.error('card not found in allcards');
       this.store.dispatch(resetFilter());
-      this.store.dispatch(showNewCard({ card: card })); //dirty trick it would be cleaner here to wait for resetFilter and then find the index and use selectSlide
+      return this.store.dispatch(showNewCard({ card: card })); //dirty trick it would be cleaner here to wait for resetFilter and then find the index and use selectSlide
     }
     this.selectSlide(index);
   }
