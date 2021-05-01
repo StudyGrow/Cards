9ecaec266922392f211f450b6d744b4dcd34375d
Rastoder Erdzan <rastoder.erdzan@rwth-aachen.de>
2021-05-01 21:02:01 +0200

    update procfile

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$NODE_ENV" == "develop" ]; then
+if [ "$NODE_ENV" == "development" ]; then
   npm install && cd angular-cards && npm install && ng build --configuration=develop
 else
   npm install && cd angular-cards && npm install && ng build --configuration=production
