703daab54c9084c9280586c7ad84d3dc6a7c7cae
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-11 01:14:54 +0200

    build

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ echo "Server install done"
 cd angular-cards 
 npm install --legacy-peer-deps
 echo "Angular install done"
+cat package.json
+cat package-lock.json
 if [ "$NODE_ENV" == "development" ]; then
   ng build --configuration=develop
 else
