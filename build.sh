e4f0f3b4b8a06c1f9c81b6b73e8145e7cc5c67a3
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-11 01:21:46 +0200

    i angular cli

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 npm install
 echo "Server install done"
 cd angular-cards 
+npm install -g @angular/cli
 npm install --legacy-peer-deps
 echo "Angular install done"
 cat package.json
-cat package-lock.json
 if [ "$NODE_ENV" == "development" ]; then
   ng build --configuration=develop
 else
