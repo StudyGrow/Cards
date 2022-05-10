cb3abf8eec310953ee3373d9c9f4eb4afcaeed5c
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-11 01:44:16 +0200

    debug

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ echo "Server install done"
 cd angular-cards 
 npm install --force
 echo "Angular install done"
-ng update @angular/cli @angular/core --allow-dirty --force
 if [ "$NODE_ENV" == "development" ]; then
   ng build --configuration=develop
 else
