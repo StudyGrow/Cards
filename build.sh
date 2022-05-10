9cbc2a3211d750f90d7e95883c2f063af11b02a7
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-05-11 00:44:01 +0200

    debug

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$NODE_ENV" == "development" ]; then
-  npm install && cd angular-cards && npm install && ng build --configuration=develop
+  npm install && cd angular-cards && npm install --force && ng build --configuration=develop
 else
-  npm install && cd angular-cards && npm install --legacy-peer-deps && ng build --configuration=production
+  npm install && cd angular-cards && npm uninstall @angular-devkit/build-angular && npm insall --legacy-peer-deps && ng build --configuration=production
 fi
