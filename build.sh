7d86a0e2c089ffd7e1fea31519776a0d06086a0c
lakhoune <a.b.lakhoune@gmail.com>
2022-05-27 18:19:05 +0200

    debug

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ npm install
 echo "Server install done"
 cd angular-cards 
 npm ci --force
+cat package-lock.json
 echo "Angular install done"
 if [ "$NODE_ENV" == "development" ]; then
   ng build --configuration=develop
