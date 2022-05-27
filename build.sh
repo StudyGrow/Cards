de082a1871ed6471b47ef704026070160d2811f3
lakhoune <a.b.lakhoune@gmail.com>
2022-05-27 19:54:58 +0200

    set node env to dev for ng build

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 npm install
 echo "Server install done"
 cd angular-cards
-npm install --force --only=dev
-echo "Angular install done"
 if [ "$NODE_ENV" == "development" ]; then
   ng build --configuration=develop
 else
+  export NODE_ENV=development
+  npm install --force --only=dev
+  echo "Angular install done"
   npm run build-ng
+  export NODE_ENV=prodcution
 fi
