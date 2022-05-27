77e343511c4b272c034d69d5c64e6080d599ca65
lakhoune <a.b.lakhoune@gmail.com>
2022-05-27 18:34:50 +0200

    force

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 npm install
 echo "Server install done"
 cd angular-cards 
-npm ci 
+npm ci --force
 echo "Angular install done"
 if [ "$NODE_ENV" == "development" ]; then
   ng build --configuration=develop
