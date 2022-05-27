aa30c84936dcbab013f0cf2e6aa36a8862f05356
lakhoune <a.b.lakhoune@gmail.com>
2022-05-27 19:08:13 +0200

    lint passes

--- angular-cards/src/app/cards/card-actions/card-actions.component.ts
+++ angular-cards/src/app/cards/card-actions/card-actions.component.ts
@@ -14,6 +14,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { CardsEffects } from 'src/app/store/effects/effects';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'card-actions',
   templateUrl: './card-actions.component.html',
   styleUrls: ['./card-actions.component.css'],
