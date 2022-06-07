98fa98cb3245c60a0b46d6b712ae57e70c6abd27
lakhoune <a.b.lakhoune@gmail.com>
2022-06-07 22:39:13 +0200

    add cron job

--- loaders/index.loader.ts
+++ loaders/index.loader.ts
@@ -1,15 +1,14 @@
-import expressLoader from './express.loader';
-import dependencyLoader from './dependency.loader';
-import databaseLoader from './database.loader';
-import logger from './logger.loader';
+import expressLoader from "./express.loader";
+import dependencyLoader from "./dependency.loader";
+import databaseLoader from "./database.loader";
+import logger from "./logger.loader";
 
 export default async ({ app }: { app: any }) => {
-  const db = await databaseLoader();
+  await databaseLoader();
   const container = await dependencyLoader({
     app: app,
     logger: logger,
-
   });
-  await expressLoader({ app: app, container: container, logger: logger });
-  logger.info('Loading complete.');
+  expressLoader({ app: app, container: container, logger: logger });
+  logger.info("Loading complete.");
 };
