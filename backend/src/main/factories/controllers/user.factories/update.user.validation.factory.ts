0ba01eec462ad064a159b35303083edd242bedd8
Erdzan Rastoder <jungerjunge2@gmail.com>
2022-08-31 18:38:41 +0200

    fix validator

--- backend/src/main/factories/controllers/user.factories/update.user.validation.factory.ts
+++ backend/src/main/factories/controllers/user.factories/update.user.validation.factory.ts
@@ -1,8 +1,4 @@
-import { EmailValidatorAdapter } from "../../../../infrastructure/validators/email.validator.adapter";
-import { PasswordValidatorAdapter } from "../../../../infrastructure/validators/password.validator.adapter";
 import { Validation } from "../../../../response/protocols/validation";
-import { OptionalEmailValidation } from "../../../../validation/validators/optional.email.validation";
-import { PasswordValidation } from "../../../../validation/validators/password.validation";
 import { RequiredFieldValidation } from "../../../../validation/validators/required.field.validation";
 import { ValidationComposite } from "../../../../validation/validators/validation.composite";
 
@@ -11,7 +7,5 @@ export const makeUpdateUserValidation = (): ValidationComposite => {
   for (const field of ["userId"]) {
     validations.push(new RequiredFieldValidation(field));
   }
-  validations.push(new OptionalEmailValidation("email", new EmailValidatorAdapter()));
-  validations.push(new PasswordValidation("password", new PasswordValidatorAdapter()));
   return new ValidationComposite(validations);
 };
