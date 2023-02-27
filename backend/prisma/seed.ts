094b9ccb51e8df4cc698467d01b8bd868fd59a96
lakhoune <ben.lakhoune@rwth-aachen.de>
2023-02-27 21:17:16 +0100

    add checks on seeding

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,6 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 async function main() {
+  // remove all data
+  await prisma.$executeRawUnsafe(`DELETE FROM "Card";`);
+  await prisma.$executeRawUnsafe(`DELETE FROM "Lecture";`);
+  await prisma.$executeRawUnsafe(`DELETE FROM "User";`);
+
   const alice = await prisma.user.upsert({
     where: { email: 'alice@example.com' },
     update: {},
@@ -27,7 +33,7 @@ async function main() {
       },
     },
   });
-  await prisma.card.upsert({
+  const card1 = await prisma.card.upsert({
     where: { id: '1' },
     update: {},
     create: {
@@ -46,7 +52,7 @@ async function main() {
       },
     },
   });
-  await prisma.card.upsert({
+  const card2 = await prisma.card.upsert({
     where: { id: '2' },
     update: {},
     create: {
@@ -65,9 +71,27 @@ async function main() {
       },
     },
   });
+  // add cards to lecture
+  await prisma.lecture.update({
+    where: { id: lecture.id },
+    data: {
+      cards: {
+        connect: [{ id: card1.id }, { id: card2.id }],
+      },
+    },
+  });
 }
 main()
   .then(async () => {
+    // check if data is in db
+    const users = await prisma.user.findMany();
+    const lectures = await prisma.lecture.findMany();
+    const cards = await prisma.card.findMany();
+
+    if (users.length !== 1) throw new Error('User count is not 1');
+    if (lectures.length !== 1) throw new Error('Lecture count is not 1');
+    if (cards.length !== 2) throw new Error('Card count is not 2');
+
     await prisma.$disconnect();
   })
   .catch(async (e) => {
